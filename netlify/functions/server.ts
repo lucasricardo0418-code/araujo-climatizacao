918dc45d818f58d0737a5afc022021e48e906e3f
lucasricardo0418-code <lucasricardo0418@gmail.com>
2026-05-27 12:59:32 -0300

    Delete netlify/functions directory

--- netlify/functions/server.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "@hono/node-server";
-import app from "../dist/server/index.js";
-
-const server = serve({
-  fetch: app.fetch,
-  port: parseInt(process.env.PORT || "3000"),
-});
-
-console.log(`Server is running on port ${server.port}`);
